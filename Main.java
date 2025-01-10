4f5850836294b17059fbb5e19842e14025eb1023
Paul <Paul-Ludwig.Simon@t-online.de>
2025-01-10 20:34:41 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //big thanks to @etylermoss for the english swear words list!
+
+        Bank bank = new Bank();
+        bank.createAccount();
+        bank.depositMoney();
+        bank.withdrawMoney();
+    }
+
+}

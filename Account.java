6d58134f16d11f8ffbad646ef28749a601016025
Paul <Paul-Ludwig.Simon@t-online.de>
2025-01-16 14:51:25 +0100

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,9 +1,15 @@
+import java.util.HashMap;
+
 public class Account {
-    private String username;
-    private String password;
-    private String userID;
+    private final String username;
+    private final String password;
+    private final String userID;
     private int balance;
 
+    // A static map to manage all accounts
+    private static HashMap<String, Account> accounts = new HashMap<>();
+
+    // Constructor
     public Account(String username, String password, String userID, int balance) {
         this.username = username;
         this.password = password;
@@ -11,23 +17,38 @@ public class Account {
         this.balance = balance;
     }
 
+    //Explain themselves
+    public String getPassword() {
+        return password;
+    }
+
     public int getBalance() {
         return balance;
     }
 
+    //deposit money 🤯
     public void depositMoney(int amount) {
         this.balance += amount;
     }
 
+    //withdraw money duhh
     public void withdrawMoney(int amount) {
-
+        this.balance -= amount;
     }
 
-    public String getUsername() {
-        return username;
+    //Check if an account exists
+    public static boolean accountExists(String username) {
+        return accounts.containsKey(username);
     }
 
-    public String getUserID() {
-        return userID;
+    //Get an account by username
+    public static Account getAccount(String username) {
+        return accounts.get(username);
+    }
+
+    //Validate login
+    public static boolean validateLogin(String username, String password) {
+        Account account = accounts.get(username);
+        return account != null && account.getPassword().equals(password);
     }
 }
